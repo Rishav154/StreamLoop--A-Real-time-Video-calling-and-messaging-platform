bde53b8de148246defbdca1c2626afc7a006c7d8
Rishav154 <rishav15045@gmail.com>
2025-06-28 18:52:14 +0530

    add chat functionality with Stream API; implement video call and chat features

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -1,11 +1,12 @@
-import {generateStreamToken} from "../lib/stream.js";
+import { generateStreamToken } from "../lib/stream.js";
 
 export async function getStreamToken(req, res) {
-    try {
-        const token = generateStreamToken(req.user.id)
-        res.status(200).json({token})
-    }catch(err){
-        console.error("Error generating stream token:", err);
-        res.status(500).json({error: "Failed to generate stream token"});
-    }
+  try {
+    const token = generateStreamToken(req.user.id);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.log("Error in getStreamToken controller:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
